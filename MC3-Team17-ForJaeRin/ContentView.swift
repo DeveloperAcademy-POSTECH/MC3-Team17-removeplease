3a22c0e26dddef478765986777e6be2ab96533f6
yuncoffee <dong072815@gmail.com>
2023-07-06 12:12:00 +0900

    Initial Commit

---
+++ MC3-Team17-ForJaeRin/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MC3-Team17-ForJaeRin
+//
+//  Created by Yun Dongbeom on 2023/07/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MC3-Team17-ForJaeRin/MC3_Team17_ForJaeRinApp.swift
@@ -0,0 +1,17 @@
+//
+//  MC3_Team17_ForJaeRinApp.swift
+//  MC3-Team17-ForJaeRin
+//
+//  Created by Yun Dongbeom on 2023/07/06.
+//
+
+import SwiftUI
+
+@main
+struct MC3_Team17_ForJaeRinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
